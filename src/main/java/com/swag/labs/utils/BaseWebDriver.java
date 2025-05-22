90b5de2ed20bf6d51e9061fa26dde7fd9a11fd63
kateryna.burla <kateryna.burla@avenga.com>
2025-05-22 10:01:00 +0300

    feat: updated pom

--- src/main/java/com/swag/labs/utils/BaseWebDriver.java
+++ src/main/java/com/swag/labs/utils/BaseWebDriver.java
@@ -18,8 +18,8 @@ public class BaseWebDriver {
                         .addArguments("--incognito")
                         .addArguments("--guest")
                         .addArguments("--no-sandbox")
-                        .addArguments("--disable-default-apps");
-                System.setProperty("webdriver.http.factory", "jdk-http-client");
+                        .addArguments("--disable-default-apps")
+                        .addArguments("--headless=new");
 
                 driver = new ChromeDriver(options);
             }
