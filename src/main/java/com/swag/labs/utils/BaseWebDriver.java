ca2b166405cda5ca4c643abbf7f57f702cb464a6
kateryna.burla <kateryna.burla@avenga.com>
2025-05-23 16:05:45 +0300

    feat: added a few updates for parallel execution

--- src/main/java/com/swag/labs/utils/BaseWebDriver.java
+++ src/main/java/com/swag/labs/utils/BaseWebDriver.java
@@ -1,36 +1,33 @@
 package com.swag.labs.utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ThreadGuard;
 
 public class BaseWebDriver {
 
-    private static WebDriver driver;
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    private BaseWebDriver() {
+    }
 
     public static WebDriver getDriver() {
-        if (driver == null) {
-            synchronized (BaseWebDriver.class) {
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--start-maximized")
-                        .addArguments("--incognito")
-                        .addArguments("--guest")
-                        .addArguments("--no-sandbox")
-                        .addArguments("--disable-default-apps")
-                        .addArguments("--headless=new");
-
-                driver = new ChromeDriver(options);
-            }
+        if (driver.get() == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized")
+                    .addArguments("--incognito")
+                    .addArguments("--guest")
+                    .addArguments("--no-sandbox")
+                    .addArguments("--disable-default-apps");
+//                        .addArguments("--headless=new")
+            driver.set(ThreadGuard.protect(new ChromeDriver(options)));
         }
-        return driver;
+        return driver.get();
     }
 
     public static void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
+        driver.get().quit();
+        driver.remove();
     }
 }
